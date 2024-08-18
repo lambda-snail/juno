6acced58cb4909e47c761d79cec3bf9dc8b84d2a
lambda-snail <>
2024-08-18 12:30:53 +0200

    fix formatting

--- src/expenses/expensesoverviewwidget.h
+++ src/expenses/expensesoverviewwidget.h
@@ -2,20 +2,28 @@
 
 #include <QWidget>
 
-namespace LambdaSnail::Juno::expenses {
-QT_BEGIN_NAMESPACE
-namespace Ui { class ExpensesOverviewWidget; }
-QT_END_NAMESPACE
+namespace LambdaSnail::Juno::expenses
+{
+    QT_BEGIN_NAMESPACE
 
-class LSExpensesOverviewWidget : public QWidget {
-Q_OBJECT
+    namespace Ui
+    {
+        class ExpensesOverviewWidget;
+    }
 
-public:
-    explicit LSExpensesOverviewWidget(QWidget *parent, class LSExpenseModel* model);
-    ~LSExpensesOverviewWidget() override;
+    QT_END_NAMESPACE
 
-private:
-    Ui::ExpensesOverviewWidget *ui;
-    LSExpenseModel* m_model;
-};
+    class LSExpensesOverviewWidget : public QWidget
+    {
+        Q_OBJECT
+
+    public:
+        explicit LSExpensesOverviewWidget(QWidget *parent, class LSExpenseModel *model);
+
+        ~LSExpensesOverviewWidget() override;
+
+    private:
+        Ui::ExpensesOverviewWidget *ui;
+        LSExpenseModel *m_model;
+    };
 } // LambdaSnail::Juno::expenses
