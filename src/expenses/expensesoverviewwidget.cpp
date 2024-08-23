b3240049e76b353e623614bf197dbd67e70407d7
lambda-snail <>
2024-08-23 16:50:55 +0200

    add button to create new expenses

--- src/expenses/expensesoverviewwidget.cpp
+++ src/expenses/expensesoverviewwidget.cpp
@@ -13,14 +13,12 @@ namespace LambdaSnail::Juno::expenses
 {
     void LSExpensesOverviewWidget::setUpToolbar(fa::QtAwesome* qtAwesome)
     {
-        searchButton = new QPushButton();
+        // Search button
+        searchButton = new QPushButton(this);
         searchButton->setText("Search");
         searchButton->setIcon(qtAwesome->icon(fa::fa_solid, fa::fa_magnifying_glass));
 
-        ui->toolBar->addWidget(searchButton);
-
-        ui->toolBar->addSeparator();
-
+        // Date selectors
         fromDate->setDisplayFormat("yyyy-MM-dd"); // TODO: Store date format in settings
         toDate->setDisplayFormat("yyyy-MM-dd");
 
@@ -32,11 +30,23 @@ namespace LambdaSnail::Juno::expenses
         fromDate->setDate(QDate(currentYear, 1, 1));
         toDate->setDate(QDate(currentYear, 12, 31));
 
+        // New Expense
+        QPushButton* newExpense = new QPushButton(this);
+        newExpense->setIcon(qtAwesome->icon(fa::fa_solid, fa::fa_square_plus));
+
+        // Build the toolbar
         ui->toolBar->addWidget(fromDate);
         ui->toolBar->addWidget(toDate);
+        ui->toolBar->addWidget(searchButton);
+        ui->toolBar->addSeparator();
+        ui->toolBar->addWidget(newExpense);
 
         // TODO: Add signals and slots to listen to date change
         connect(fromDate, &QDateEdit::dateChanged, this, &LSExpensesOverviewWidget::onSearchDatesChanged);
+        connect(newExpense, &QPushButton::pressed, this, [&]()
+        {
+            m_model->insertRow(0);
+        });
     }
 
     LSExpensesOverviewWidget::LSExpensesOverviewWidget(QWidget *parent, LSExpenseModel* model, fa::QtAwesome* qtAwesome) : QWidget(parent), ui(new Ui::ExpensesOverviewWidget), m_model(model)
@@ -50,6 +60,7 @@ namespace LambdaSnail::Juno::expenses
 
         using ExpenseColumns = shared::LSDatabaseManager::ExpenseColumns;
 
+        // TODO: Listen to column size changes and store somewhere for persistence between sessions
         ui->tableView->setModel(model);
         ui->tableView->setColumnHidden(static_cast<int>(ExpenseColumns::id), true);
         ui->tableView->setColumnHidden(static_cast<int>(ExpenseColumns::createdOn), true);
