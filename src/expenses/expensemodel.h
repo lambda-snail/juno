6c0d638e0edbca05cabfd6514df77b44bc4f8a59
lambda-snail <>
2024-09-07 14:26:24 +0200

    remove deprecated comment

--- src/expenses/expensemodel.h
+++ src/expenses/expensemodel.h
@@ -4,7 +4,6 @@
 
 namespace LambdaSnail::Juno::expenses
 {
-    // TODO: Take date as params in ctor so that we don't query everything at once
     class LSExpenseModel : public LSExpenseModelBase
     {
     public:
