3c0e7025e98b66c76bfe502759116c7975c1d4ea
lambda-snail <>
2024-09-19 18:39:41 +0200

    chore: cleanup (now) useless comment

--- src/recurring_expenses/recurringexpensesoverview.h
+++ src/recurring_expenses/recurringexpensesoverview.h
@@ -16,15 +16,6 @@
 
 namespace LambdaSnail::Juno::expenses
 {
-    // QT_BEGIN_NAMESPACE
-    //
-    // namespace Ui
-    // {
-    //     class RecurringExpensesOverview;
-    // }
-    //
-    // QT_END_NAMESPACE
-
     class LSRecurringExpensesOverview : public QWidget
     {
         Q_OBJECT
